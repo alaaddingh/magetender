5cb23f8bf3439fcfbf8db5b5c251a90ac6f35ac9
Dguerr15 <dguerr15@ucsc.edu>
2026-03-05 15:10:26 -0800

    buttons and floating text working

--- Assets/Scripts/Managers/SceneLocalization.cs
+++ Assets/Scripts/Managers/SceneLocalization.cs
@@ -1,43 +1,11 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/* Localizes UI by matching TMP text to a single list of (scene, matchText, key, style). One FindObjectsOfType per scene load.
-   Optional: add LocalizedTMPText to labels in the editor with Key + Style set; then this scene scanner can be removed. */
+/* Runs on scene load to cap mood graph quadrant label font sizes in MixScene. All other UI is localized via LocalizedTMPText in the editor. */
 public class SceneLocalization : MonoBehaviour
 {
-	[Serializable]
-	public struct LocalizedEntry
-	{
-		public string sceneName;
-		public string matchText;
-		public string key;
-		public LocalizedTMPText.Style style;
-	}
-
 	private static SceneLocalization s_instance;
-
-	private static readonly LocalizedEntry[] Entries =
-	{
-		new LocalizedEntry { sceneName = "TitleScreen", matchText = "Start", key = "start_button", style = LocalizedTMPText.Style.Button },
-		new LocalizedEntry { sceneName = "TitleScreen", matchText = "Back", key = "back_button", style = LocalizedTMPText.Style.Button },
-		new LocalizedEntry { sceneName = "TitleScreen", matchText = "Options", key = "options_button", style = LocalizedTMPText.Style.Button },
-		new LocalizedEntry { sceneName = "TitleScreen", matchText = "Credits", key = "credits_button", style = LocalizedTMPText.Style.Button },
-		new LocalizedEntry { sceneName = "MixScene", matchText = "Begin Brew", key = "begin_brew_button", style = LocalizedTMPText.Style.Button },
-		new LocalizedEntry { sceneName = "MixScene", matchText = "Next Day", key = "next_day_button", style = LocalizedTMPText.Style.Button },
-		new LocalizedEntry { sceneName = "MixScene", matchText = "Next", key = "next_button", style = LocalizedTMPText.Style.Button },
-		new LocalizedEntry { sceneName = "MixScene", matchText = "Back", key = "back_button", style = LocalizedTMPText.Style.Button },
-		new LocalizedEntry { sceneName = "MixScene", matchText = "Mood Profile", key = "mood_profile_title", style = LocalizedTMPText.Style.MoodGraphTitleOrDescription },
-		new LocalizedEntry { sceneName = "MixScene", matchText = "Hover icons for mood details", key = "mood_graph_hint", style = LocalizedTMPText.Style.MoodGraphHint },
-		new LocalizedEntry { sceneName = "MixScene", matchText = "Adjust your customer's mood", key = "mood_graph_description", style = LocalizedTMPText.Style.MoodGraphTitleOrDescription },
-		new LocalizedEntry { sceneName = "MixScene", matchText = "Continue", key = "continue_button", style = LocalizedTMPText.Style.Button },
-		new LocalizedEntry { sceneName = "QTECombatScene", matchText = "ATTACK", key = "attack_label", style = LocalizedTMPText.Style.None },
-		new LocalizedEntry { sceneName = "QTECombatScene", matchText = "DEFEND", key = "defend_label", style = LocalizedTMPText.Style.None },
-		new LocalizedEntry { sceneName = "QTECombatScene", matchText = "PLAYER", key = "player_label", style = LocalizedTMPText.Style.None },
-		new LocalizedEntry { sceneName = "QTECombatScene", matchText = "CUSTOMER", key = "customer_label", style = LocalizedTMPText.Style.None },
-		new LocalizedEntry { sceneName = "QTECombatScene", matchText = "To Bar", key = "to_bar_button", style = LocalizedTMPText.Style.Button }
-	};
 
 	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
 	private static void EnsureInstance()
@@ -83,32 +51,6 @@ public class SceneLocalization : MonoBehaviour
 	private void LocalizeActiveScene()
 	{
 		string sceneName = SceneManager.GetActiveScene().name;
-		TMP_Text[] allTexts = UnityEngine.Object.FindObjectsOfType<TMP_Text>(true);
-
-		for (int e = 0; e < Entries.Length; e++)
-		{
-			if (Entries[e].sceneName != sceneName)
-			{
-				continue;
-			}
-
-			for (int t = 0; t < allTexts.Length; t++)
-			{
-				TMP_Text tmp = allTexts[t];
-				if (tmp.text.Trim() != Entries[e].matchText)
-				{
-					continue;
-				}
-
-				LocalizedTMPText loc = tmp.GetComponent<LocalizedTMPText>();
-				if (loc == null)
-				{
-					loc = tmp.gameObject.AddComponent<LocalizedTMPText>();
-				}
-
-				loc.SetKeyAndStyle(Entries[e].key, Entries[e].style);
-			}
-		}
 
 		if (sceneName == "MixScene")
 		{
