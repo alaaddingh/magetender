6d48870cb5e730b34fb8e17ed7d68cdd36d6413a
alaaddingh <alagho03@gmail.com>
2026-04-17 17:02:50 -0700

    bg art, visuals for timer, fixed coincanvas layering for shop

--- Assets/Scripts/UI/TimerUi.cs
+++ Assets/Scripts/UI/TimerUi.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerUI : MonoBehaviour
 {
@@ -11,21 +12,15 @@ public class TimerUI : MonoBehaviour
     
     public bool TimeUp = false;
     
-
-
     public TextMeshProUGUI timerText;
+    public Image ClockImg;
+    private Vector3 clockOriginalPos;
 
     [Header("Panels")]
     [SerializeField] private GameObject TimerPanel;
     [SerializeField] private GameObject BasePanel;
     [SerializeField] private GameObject IngredientsPanel;
     [SerializeField] private GameObject ServePanel;
-
-
-
-
-    
-
 
     void OnEnable()
     {
@@ -60,10 +55,26 @@ public class TimerUI : MonoBehaviour
     {
         timeRemaining = duration;
         isRunning = true;
+        timerText.color = Color.white;
+        clockOriginalPos = ClockImg.rectTransform.localPosition;
     }
 
     void UpdateUI()
     {
         timerText.text = Mathf.Ceil(timeRemaining).ToString();
+        timerText.color = timeRemaining <= 10f ? Color.red : Color.white;
+            if (timeRemaining > 0f && timeRemaining <= 10f)
+                ShakeClockImage();
+          
+    }
+
+    void ShakeClockImage()
+    {
+        float intensity = 200f;
+        Vector3 shakeOffset = new Vector3(
+            Random.Range(-intensity, intensity),
+            Random.Range(-intensity, intensity),
+            0f) * 0.02f;
+        ClockImg.rectTransform.localPosition = clockOriginalPos + shakeOffset;
     }
 }
